6de89dc48403fc11110dc1559e9f940c96a43f7e
AllForTheDea <dragonfleet@gmail.com>
2020-04-06 06:05:22 -0400

    Added service class and stuff

--- Angular app/Test/src/app/app-routing.module.ts
+++ Angular app/Test/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ExtraComponentComponent } from './extra-component/extra-component.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'second', component: ExtraComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
